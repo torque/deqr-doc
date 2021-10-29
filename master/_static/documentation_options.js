f7232f94b9e9b0c263150d0c598c826f40741437
torque <torque@users.noreply.github.com>
2021-10-29 16:45:41 +0000

    Commit torque/deqr@0ba855dcfc7892e602b3b0e866507c2a1c73ded0

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0.post3+fb19e16',
+    VERSION: '0.2.0.post4+0ba855d',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
